01ace6c2574456053c5a527e7827a5a78f739bd5
AmmarQD <42525558+AmmarQD@users.noreply.github.com>
2018-08-26 20:11:27 +0100

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  const body = document.querySelector('body');
+
+  if (body.classList.contains('t--light')) {
+    body.classList.remove('t--light');
+    body.classList.add('t--dark');
+  }
+  else {
+    body.classList.remove('t--dark');
+    body.classList.add('t--light');
+  }
+;
+}
